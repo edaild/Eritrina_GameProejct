f12a2d8151176a7897ab7eb8f19995cce250f685
edaild <ch5183sb@gmail.com>
2024-10-11 23:36:00 +0900

    Feat : 프로토 타입 메인 캐릭터 배치 및 스크립트 적용

--- Assets/Script/Cheracter/NPC/NpcCharacerController.cs
+++ Assets/Script/Cheracter/NPC/NpcCharacerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class NpcCharacerController : MonoBehaviour
 {
+    public GameObject[] NpcCharacter;
+    public GameObject fKeyUI;
+    public float NpcSpeed;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,16 @@ public class NpcCharacerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                // 각자 스크립트에서 상효작용이 진행된다. 물건일 경우 인벤토리 창에 추가 대화 일 경우 대화창 활성화
+            }
+        }
     }
 }
