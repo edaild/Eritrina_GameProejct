463af4d432d1a42eeb34b21763eedddd0505f50f
edaild <ch5183sb@gmail.com>
2024-10-19 23:48:25 +0900

    Feat :Lobby Scene 이동 추가

--- Assets/Script/Cheracter/NPC/NpcCharacerController.cs
+++ Assets/Script/Cheracter/NPC/NpcCharacerController.cs
@@ -8,7 +8,8 @@ public class NpcCharacterController : MonoBehaviour
     [Header("NPC system")]
     public GameObject[] NpcCharacter; // NPC 캐릭터 배열
 
-   // public GameObject FKeyUI; // F 키 안내 UI
+    public GameObject functionUI; // F 키 안내 UI
+    public Text functiontext;
     public float NpcSpeed; // NPC 속도 (현재 사용되지 않음)
 
     public GameObject textUI; // 대화 UI
@@ -38,9 +39,11 @@ public class NpcCharacterController : MonoBehaviour
                 // 현재 오브젝트가 "NPC" 태그인 경우 대화 시작
                 if (transform.CompareTag("NPC") && text_Count == 0)
                 {
+                    functionUI.SetActive(false);
                     textUI.SetActive(true); // 대화 UI 활성화
                     hpbar. SetActive(false); // HP 바 비활성화
                     menuButton.SetActive(false); // 메뉴 버튼 비활성화
+                    functionUI.SetActive(false);
                     npcName.text = "아이"; // NPC 이름 설정
                     textObject.text = "안녕하세요."; // 첫 번째 대화 내용
                     text_Count = 1; // 대화 카운트 설정
@@ -64,6 +67,7 @@ public class NpcCharacterController : MonoBehaviour
                 text_Count = 0; // 대화 카운트 초기화
                 NpcCheck = false; // 대화 불가능으로 설정
                 hpbar. SetActive(true); // player HP 바 활성화
+               functionUI.SetActive(true);
             }
         }
         else
@@ -78,6 +82,8 @@ public class NpcCharacterController : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Debug.Log("대화를 하려면 F 키를 누르세요");
+            functionUI.SetActive(true);
+            functiontext.text = "대화   F";
             NpcCheck = true; // NPC와의 대화 가능으로 설정
         }
     }
@@ -90,6 +96,7 @@ public class NpcCharacterController : MonoBehaviour
             NpcCheck = false; // 대화 불가능으로 설정
             textUI.SetActive(false); // 대화 UI 비활성화
             hpbar.SetActive(true); // Player Hp바를 활성화
+            functionUI.SetActive(false);
             text_Count = 0; // 대화 카운트 초기화
         }
     }
---
+++ Assets/Script/Game System/System/SceneLoad.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    [Header("Function UI")]
+    public GameObject functionUI;
+    public Text functionText;
+    private bool LobbyIn = false;
+
+    private void Update()
+    {
+        // 플레이어가 트리거 안에 있을 때 F 키 입력 체크
+        if (LobbyIn && Input.GetKeyDown(KeyCode.F))
+        {
+            LoadScene("LobbyScene"); 
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (transform.CompareTag("Lobby In"))
+            {
+                LobbyIn = true;
+                functionUI.SetActive(true);
+                functionText.text = "로비로 이동  F";
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            LobbyIn = false;
+            functionUI.SetActive(false);
+        }
+    }
+
+    // 씬 로드 메서드
+    public void LoadScene(string sceneName)
+    {
+        Debug.Log($"{sceneName} 씬 로드 중");
+        SceneManager.LoadScene(sceneName);
+    }
+}
+
