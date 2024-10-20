8225eda33a10ead95f3fdb1e2ecffa6c005d7fb2
edaild <ch5183sb@gmail.com>
2024-10-20 20:35:20 +0900

    Feat : 20241020

--- Assets/Script/Game System/System/SceneLoad.cs
+++ Assets/Script/Game System/System/SceneLoad.cs
@@ -9,14 +9,20 @@ public class SceneLoad : MonoBehaviour
     [Header("Function UI")]
     public GameObject functionUI;
     public Text functionText;
+
     private bool LobbyIn = false;
+    private bool LobbyInOut = false;
 
     private void Update()
     {
         // 플레이어가 트리거 안에 있을 때 F 키 입력 체크
         if (LobbyIn && Input.GetKeyDown(KeyCode.F))
         {
-            LoadScene("LobbyScene"); 
+            LoadScene("LobbyScene");
+        }
+        if (LobbyInOut && Input.GetKeyDown(KeyCode.F))
+        {
+            LoadScene("HotelScene");
         }
     }
 
@@ -30,6 +36,13 @@ public class SceneLoad : MonoBehaviour
                 functionUI.SetActive(true);
                 functionText.text = "로비로 이동  F";
             }
+
+            if (transform.CompareTag("Lobby Out"))
+            {
+                LobbyInOut = true;
+                functionUI.SetActive(true);
+                functionText.text = "호텔 외부로 이동  F";
+            }
         }
     }
 
@@ -38,6 +51,7 @@ public class SceneLoad : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             LobbyIn = false;
+            LobbyInOut = false;
             functionUI.SetActive(false);
         }
     }
