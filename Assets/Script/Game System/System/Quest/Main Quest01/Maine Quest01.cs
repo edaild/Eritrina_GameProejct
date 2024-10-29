53ae7b8a932bb9e0c3826e18f4d51a45b003adee
edaild <ch5183sb@gmail.com>
2024-10-29 11:49:30 +0900

    FIx MainQuest 코드 가독성을 위해 분리

--- Assets/Script/Game System/System/Quest/Main Quest01/Maine Quest01.cs
+++ Assets/Script/Game System/System/Quest/Main Quest01/Maine Quest01.cs
@@ -16,7 +16,7 @@ public class MaineQuest01 : MonoBehaviour
     public GameObject textUI; // 대화 UI
     public Text npcName; // NPC 이름을 표시할 UI 텍스트
     public Text textObject; // 대화 내용을 표시할 UI 텍스트
-    public GameObject hpbar; // HP 바 (현재 비활성화)
+    public GameObject PlayerHpbar; // HP 바 (현재 비활성화)
     public GameObject menuButton; // 메뉴 버튼 (현재 비활성화)
 
     public bool NpcCheck; // NPC와의 대화 가능 여부
@@ -43,6 +43,23 @@ public class MaineQuest01 : MonoBehaviour
         NpcCharacterCheck(); // NPC 대화 체크
     }
 
+    private void StartConversation()                // 대화 시작시
+    {
+        functionUI.SetActive(false);                // F 키 안내 UI 비활성화
+        textUI.SetActive(true);                     // 대화 UI 활성화
+        PlayerHpbar.SetActive(false);                     // HP 바 비활성화
+        menuButton.SetActive(false);                // 메뉴 버튼 비활성화
+    }
+    private void EndConversation()                                       // 대화 종료시
+    {
+        textUI.SetActive(false);                                        // 모든 대화가 끝나면 UI 비활성화
+        text_Count = 0;                                                 // 대화 카운트 초기화
+        NpcCheck = false;                                               // 대화 불가능으로 설정
+        PlayerHpbar.SetActive(true);                                    // Player HP 바 활성화
+        functionUI.SetActive(true);                                    // F 키 안내 UI 활성화
+        menuButton.SetActive(true);                                    // 메뉴 버튼 활성화
+
+    }
     private void NpcCharacterCheck()
     {
         if (NpcCheck) // NPC와 대화 가능 상태일 때
@@ -52,10 +69,7 @@ public class MaineQuest01 : MonoBehaviour
                 // 현재 오브젝트가 "NPC" 태그인 경우 대화 시작
                 if (transform.CompareTag("NPC01") && text_Count == 0)
                 {
-                    functionUI.SetActive(false);                // F 키 안내 UI 비활성화
-                    textUI.SetActive(true);                     // 대화 UI 활성화
-                    hpbar.SetActive(false);                     // HP 바 비활성화
-                    menuButton.SetActive(false);                // 메뉴 버튼 비활성화
+                    StartConversation();
                     npcName.text = "수상한 소녀";                     // NPC 이름 설정
                     textObject.text = "이 호텔은 먼가를 숨기고 있어";              // 첫 번째 대화 내용
                     text_Count = 1;                             // 대화 카운트 설정
@@ -79,19 +93,13 @@ public class MaineQuest01 : MonoBehaviour
                 }
                 else if (text_Count == 2)
                 {
-                    textObject.text = "아 투숙객 이군요 즐거운 휴식 되세요.";                            // 세 번째 대화 내용
-                    text_Count = 3;                                                     // 대화 카운트 증가
+                    textObject.text = "아 투숙객 이군요 즐거운 휴식 되세요.";          // 세 번째 대화 내용
+                    text_Count = 3;                                                   // 대화 카운트 증가
                 }
                 else if (text_Count == 3)
                 {
-                    textUI.SetActive(false);                                           // 모든 대화가 끝나면 UI 비활성화
-                    text_Count = 0;                                                    // 대화 카운트 초기화
-                    NpcCheck = false;                                                  // 대화 불가능으로 설정
-                    hpbar.SetActive(true);                                             // Player HP 바 활성화
-                    functionUI.SetActive(true);                                        // F 키 안내 UI 활성화
-                    menuButton.SetActive(true);                                        // 메뉴 버튼 활성화
-
-                    FindObjectOfType<QuestManager>().CompleteCurrentStep();           // 퀘스트 단계 완료
+                    EndConversation();
+                    FindObjectOfType<QuestManager>().CompleteCurrentStep();          // 퀘스트 단계 완료
                     NpcQuestCount = 1;                                              // 쿼스트 단계를 다음 단계로 올린다.
 
                 }
@@ -102,10 +110,7 @@ public class MaineQuest01 : MonoBehaviour
                 // 현재 오브젝트가 "NPC" 태그인 경우 대화 시작
                 if (transform.CompareTag("NPC02") && text_Count == 0)
                 {
-                    functionUI.SetActive(false);                // F 키 안내 UI 비활성화
-                    textUI.SetActive(true);                     // 대화 UI 활성화
-                    hpbar.SetActive(false);                     // HP 바 비활성화
-                    menuButton.SetActive(false);                // 메뉴 버튼 비활성화
+                    StartConversation();
                     npcName.text = "호텔 직원";                     // NPC 이름 설정
                     textObject.text = "에리트리나 호텔에 오신걸 환영합니다.";              // 첫 번째 대화 내용
                     text_Count = 1;                             // 대화 카운트 설정
@@ -134,43 +139,13 @@ public class MaineQuest01 : MonoBehaviour
                 }
                 else if (text_Count == 3)
                 {
-                    textUI.SetActive(false);                                           // 모든 대화가 끝나면 UI 비활성화
-                    text_Count = 0;                                                    // 대화 카운트 초기화
-                    NpcCheck = false;                                                  // 대화 불가능으로 설정
-                    hpbar.SetActive(true);                                             // Player HP 바 활성화
-                    functionUI.SetActive(true);                                        // F 키 안내 UI 활성화
-                    menuButton.SetActive(true);                                        // 메뉴 버튼 활성화
+                    EndConversation();
 
                     FindObjectOfType<QuestManager>().CompleteCurrentStep();           // 퀘스트 단계 완료
                     NpcQuestCount = 2;                                              // 쿼스트 단계를 다음 단계로 올린다.
 
                 }
             }
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        // Player와 충돌 시 대화 가능
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("대화를 하려면 F 키를 누르세요");
-            functionUI.SetActive(true);                             // F 키 안내 UI 활성화
-            functiontext.text = "대화   F";                         // 안내 텍스트 설정
-            NpcCheck = true;                                        // NPC와의 대화 가능으로 설정           
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        // Player가 NPC의 범위를 벗어날 때
-        if (other.CompareTag("Player"))
-        {
-            NpcCheck = false;                                   // 대화 불가능으로 설정
-            textUI.SetActive(false);                            // 대화 UI 비활성화
-            hpbar.SetActive(true);                              // Player HP 바 활성화
-            functionUI.SetActive(false);                        // F 키 안내 UI 비활성화
-            text_Count = 0;                                     // 대화 카운트 초기화
         }
     }
 }
